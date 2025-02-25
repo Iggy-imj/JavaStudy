57b62bfb13e5be71210c8851ed7d2fc87f58a54e
IhorK <iggy.imj@gmail.com>
2025-02-25 18:29:59 +0100

    add 3 methods in Auto class: Broken, Drive, Repair

--- src/hw_21/Avto.java
+++ src/hw_21/Avto.java
@@ -7,6 +7,7 @@ public class Avto {
     String fuel;
     double engine;
     int power;
+    boolean isBroken;
 
     public Avto(String brand, String model, String colour, String fuel, double engine, int power){
         this.brand = brand;
@@ -15,6 +16,7 @@ public class Avto {
         this.fuel = fuel;
         this.engine = engine;
         this.power = power;
+        this.isBroken = false;
     }
     void about() {
         System.out.println("CAR DESCRIPTION\n" +
@@ -24,6 +26,24 @@ public class Avto {
                 "  Fuel: " + fuel + "\n" +
                 "  Engine: " + engine + "\n" +
                 "  Power: " + power);
+    }
+
+    void drive() {
+        if (isBroken) {
+            System.out.println("The car is broken and can't move!");
+        } else {
+            System.out.println(brand + " " + model + " is driving forward!");
+        }
+    }
+
+    void breakDown() {
+        isBroken = true;
+        System.out.println(brand + " " + model + " is broken!");
+    }
+
+    void repair() {
+        isBroken = false;
+        System.out.println(brand + " " + model + " has been repaired!");
     }
 
 }
--- src/hw_21/MainAvto.java
+++ src/hw_21/MainAvto.java
@@ -6,5 +6,10 @@ public class MainAvto {
         Avto avto = new Avto("VW", "Golf GTD MK7", "Violet", "Diesel", 2.0, 180);
         avto.about();
 
+        avto.breakDown();
+        avto.drive();
+        avto.repair();
+        avto.drive();
+
     }
 }
