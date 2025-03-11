8e2b66bf9b730def93f659728bcd749d35a953d5
IhorK <iggy.imj@gmail.com>
2025-03-11 20:27:28 +0100

    lists

---
+++ src/lists/App.java
@@ -0,0 +1,17 @@
+package lists;
+
+public class App {
+    public static void main(String[] args) {
+        MyArrayList<String> stringArray = new MyArrayList<>();
+        stringArray.add("Hello");
+        stringArray.add("World");
+        System.out.println(stringArray);
+
+        MyArrayList<Integer> intArray = new MyArrayList<>();
+        intArray.add(100);
+        intArray.add(200);
+        System.out.println(intArray);
+
+
+    }
+}
